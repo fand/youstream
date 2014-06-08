b77b17296b736f3640f8052b5569fa231bb9d79c
fand <fand>
2014-06-08 22:16:41 +0900

    test runs

--- test/youstream.js
+++ test/youstream.js
@@ -1,4 +1,4 @@
-var vows = require('vows');
+var mocha = require('mocha');
 var assert = require('assert');
 var fs = require('fs');
 var path   = require('path');
@@ -17,95 +17,96 @@ var md5_hex = function (src) {
 };
 
 
-vows.describe('download').addBatch((function () {
+describe('download:', function(){
 
   var tests = {};
   var sites = JSON5.parse('' + fs.readFileSync('./test/sites.json5'));
   var tmpfile_id = 0;
 
   _.each(sites, function (site, sitename) {
-    _.each(site, function (video, i) {
 
-      tests['video from ' + sitename + ': #' + i] = (function(video){
+    describe(sitename, function(){
 
-        var filename;
-        if (_.has(video, 'file')) {
-          filename = video.file;
-        }
-        else if (_.has(video, 'ext')) {
-          filename = tmpfile_id+ + video.ext;
-        }
-        else {
-          filename = tmpfile_id++ + 'tmp';
-        }
-        var filepath = path.join(__dirname, 'tmp', filename);
+      _.each(site, function (video, i) {
 
-        // Prepare options.
-        var options = [];
-        if (_.has(video, 'params')) {
-          if (_.has(video.params, 'videopassword')) {
-            options = options.concat(['--video-password', video.params.videopassword]);
-          }
-          if (_.has(video.params, 'username')) {
-            options = options.concat(['--username', video.params.username]);
-          }
-          if (_.has(video.params, 'password')) {
-            options = options.concat(['--password', video.params.password]);
-          }
-        }
+        describe('video #' + i, function () {
 
-        var test = {
-          topic: function() {
+          var filename;
+          var filepath;
+          var expected;
+
+          before(function(done) {
+
+            if (_.has(video, 'file')) {
+              filename = video.file;
+            }
+            else if (_.has(video, 'ext')) {
+              filename = tmpfile_id+ + video.ext;
+            }
+            else {
+              filename = tmpfile_id++ + 'tmp';
+            }
+            filepath = path.join(__dirname, 'tmp', filename);
+
+            // Prepare options.
+            var options = [];
+            if (_.has(video, 'params')) {
+              if (_.has(video.params, 'videopassword')) {
+                options = options.concat(['--video-password', video.params.videopassword]);
+              }
+              if (_.has(video.params, 'username')) {
+                options = options.concat(['--username', video.params.username]);
+              }
+              if (_.has(video.params, 'password')) {
+                options = options.concat(['--password', video.params.password]);
+              }
+            }
+
             var dl = youstream(video.url, options);
-            var cb = this.callback;
 
             dl.pipe(fs.createWriteStream(filepath, 'binary'));
-            dl.on('error', cb);
+            dl.on('error', done);
             dl.on('end', function () {
-              cb(null, filename, filepath, video);
+              expected = video;
+              done();
             });
 
-          },
-          'file downloaded correctly': function(err, filename, filepath, expected) {
-            if (err) { throw err; }
+          });
 
-            // Check existance.
+          it('should exist as a file', function(){
             var exists = fs.existsSync(filepath);
-            assert.isTrue(exists, 'downloaded file exists.');
+            assert(exists, 'downloaded file exists.');
+          });
 
-            if (exists) {
-
-              // Check md5 hash.
-              if (_.has(video, 'md5')) {
-                var raw = fs.readFileSync(filepath, 'binary');
-                var head = raw.substr(0, 10241);
-                var md5 = md5_hex(head);
-                assert.equal(md5, video.md5, 'md5sum correct.');
-              }
-
-              // Check filename.
-              if (_.has(video, 'file')) {
-                assert.equal(filename, video.file, 'filename correct.');
-              }
-
-              // Delete file after each test.
-              fs.unlinkSync(filepath);
-
-            } else {
-              assert.isTrue(exists);
+          it('should correct md5sum', function() {
+            if (_.has(video, 'md5')) {
+              var raw = fs.readFileSync(filepath, 'binary');
+              var head = raw.substr(0, 10241);
+              var md5 = md5_hex(head);
+              assert.equal(md5, video.md5, 'md5sum correct.');
             }
-          }
-        };
+            else {
+              assert(true, 'no md5sum info given.');
+            }
+          });
 
-        return test;
-      })(video);
+          it('should have correct filename', function () {
+            // Check filename.
+            if (_.has(video, 'file')) {
+              assert.equal(filename, video.file, 'filename correct.');
+            }
+          });
 
+          after(function () {
+            // Delete file after each test.
+            fs.unlinkSync(filepath);
+          });
+
+        });
+      });
     });
   });
-
-  return tests;
-
-}).bind(this)()).export(module);
+});
 
 
 // Show the error when vows test fails ailently.
